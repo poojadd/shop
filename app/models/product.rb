f7a44adcd07bbdb83c5f30deb542db0815b64bd6
PDudhatra <pooja.dudhatra@anchanto.com>
2017-06-29 01:50:01 +0530

    Validation added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   self.inheritance_column = :type
 
+  validates_presence_of :name, :price, :status, :inward_date, :type, :description
+  validates_numericality_of :price, :greater_than => 0
   scope :pens, -> { where(type: 'Pen') }
   scope :notebooks, -> { where(type: 'Notebook') }
   def self.types
