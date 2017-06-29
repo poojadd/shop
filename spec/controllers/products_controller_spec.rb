31ea4092cd3424718aefe58184cb8f6bb419a465
PDudhatra <pooja.dudhatra@anchanto.com>
2017-06-29 22:37:05 +0530

    index spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ProductsController, type: :controller do
     end
 
     it "assigns @products" do
-      product = Product.create
+      product = Product.create(:name=>'pen', :price=>10, :color=>'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
       get :index
       expect(assigns(:products)).to eq([product])
     end
@@ -19,4 +19,5 @@ RSpec.describe ProductsController, type: :controller do
       expect(response).to render_template("index")
     end
   end
+
 end
