b5b78a311fb59b6630c10ee71f443c299482426d
PDudhatra <pooja.dudhatra@anchanto.com>
2017-06-29 23:56:59 +0530

    specs for product

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ProductsController, type: :controller do
     end
 
     it "assigns @products" do
-      product = Product.create#(:name=>'pen', :price=>10, :color=>'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
+      product = Product.create(name: 'pen', price: 10, color: 'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
       get :index
       expect(assigns(:products)).to eq([product])
     end
@@ -21,13 +21,14 @@ RSpec.describe ProductsController, type: :controller do
   end
 
   describe "GET show" do
-    product = Product.create(:name=>'pen', :price=>10, :color=>'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
     it "render show templet" do
-      get :show, {:id => product}
+      product = Product.create(name: 'pen', price: 10, color: 'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
+      get :show, {id: product}
       response.should render_template('show')
     end
     it "assigns the requested product as @product" do
-      get :show, {:id => product.to_param}
+      product = Product.create(name: 'pen', price: 10, color: 'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
+      get :show, {id: product.to_param}
       assigns(:product).should eq(product)
     end
   end
@@ -46,18 +47,113 @@ RSpec.describe ProductsController, type: :controller do
     end
   end
   describe "GET edit" do
-    product = Product.create(:name=>'pen', :price=>10, :color=>'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
+
     it "render edit template" do
-      get :edit, {:id => product}
+      product = Product.create(name: 'pen', price: 10, color: 'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
+      get :edit, {id: product}
       response.should render_template('edit')
     end
 
-    it "assigns the requested tree as @tree" do
-      get :edit, {:id => product}
+    it "assigns the requested product as @product" do
+      product = Product.create(name: 'pen', price: 10, color: 'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen')
+      get :edit, {id: product}
       assigns(:product).should_not be_nil
       assigns(:product).should eq(product)
     end
+  end
+  describe "POST create" do
+    before do
+      @product_params = {product:
+                          {name: 'pen', price: 10, color: 'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen'
+                          }
+      }
+    end
+    it "assign a @product variable" do
+      post :create, @product_params
+      assigns[:product].should_not be_nil
+      assigns[:product].should be_kind_of(Product)
+    end
+    context "when Suceessfull" do
+      subject {
+        post :create, @product_params
+      }
+      it "redirects to show page" do
+        subject
+        response.should redirect_to(assigns[:product])
+      end
 
+      it "creates product record" do
+        lambda {
+          subject
+        }.should change(Product, :count).by(1)
 
+      end
+    end
+    context "when faillure" do
+      before do
+        @product_params[:product][:name]=''
+      end
+      it "re-renders new templet" do
+        post :create, @product_params
+        response.should render_template('new')
+      end
+
+      it "does not create person record" do
+        lambda {
+          post :create, @product_params
+        }.should_not change(Product, :count).by(1)
+        assigns[:product].errors.get(:name).should_not be_nil
+      end
+    end
+
+  end
+
+  describe "PUT update" do
+
+    before do
+      @product_params =
+          {name: 'pen', price: 10, color: 'red', status:1, inward_date:'12/12/2016',type:'Pen',description:'best pen'
+          }
+      @new_paams={name:'New Pen'}
+    end
+    it "assigns value to product" do
+      product = Product.create(@product_params)
+      put :update, {id: product, product: @new_paams}
+      assigns[:product].should_not be_nil
+      assigns[:product].should be_kind_of(Product)
+    end
+    context "when Suceessfull" do
+      it "redirects to show page" do
+        product = Product.create(@product_params)
+        put :update, {id: product, product: @product_params}
+        response.should redirect_to(assigns[:product])
+      end
+      it "update product record" do
+        product = Product.create(@product_params)
+        put :update, {id: product, product: @product_params}
+        assigns[:product].name.should == @product_params[:name]
+      end
+    end
+    context "when faillure" do
+      it "re-render to show page" do
+        product = Product.create(@product_params)
+        @product_params[:name] = ''
+        put :update, {id: product, product: @product_params}
+        response.should render_template('edit')
+      end
+      it " not update product record" do
+        product = Product.create(@product_params)
+        @product_params[:name] = nil
+        new_params ={name:'',price:nil,status:nil,inward_date:nil, type:nil, description:nil}
+        put :update, {id: product, product: new_params}
+        assigns[:product].errors.get(:name).should_not be_nil
+        assigns[:product].errors.get(:price).should_not be_nil
+        assigns[:product].errors.get(:status).should_not be_nil
+        assigns[:product].errors.get(:inward_date).should_not be_nil
+        assigns[:product].errors.get(:type).should_not be_nil
+        assigns[:product].errors.get(:description).should_not be_nil
+      end
+
+    end
   end
 end
